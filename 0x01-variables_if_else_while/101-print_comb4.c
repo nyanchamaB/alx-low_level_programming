81988e492bbec30d361b4dc2ad8095312a86e048
nyanchamaB <nyanchamabrendah3@gmail.com>
2023-07-16 15:03:45 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,26 +8,26 @@
  */
 int main(void)
 {
-int i, j, k;
+	int i, j, k;
 
-for (i = 0; i < 8; i++)
-{
-	for (j = i + 1; j < 9; j++)
+	for (i = 0; i < 8; i++)
 	{
-		for (k = j + 1; k < 10; k++)
+		for (j = i + 1; j < 9; j++)
 		{
-		putchar((i) + '0');
-		putchar((j) + '0');
-		putchar((k) + '0');
+			for (k = j + 1; j < 10; k++)
+			{
+				putchar((i) + '0');
+				putchar((j) + '0');
+				putchar((k) + '0');
 
-		if (!(i == 7 && j == 8 && k == 9))
-				{
-				putchar(',');
-				putchar(' ');
-				}
+				if (!(i == 7 && j == 8 && k == 9))
+						{
+						putchar(',');
+						putchar(' ');
+						}
+			}
 		}
 	}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
