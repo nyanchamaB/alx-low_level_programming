505e6dd5500508f949004af45b1c1724a2b8ad52
nyanchamaB <nyanchamabrendah3@gmail.com>
2023-07-18 17:16:51 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include < stdio.h>
+
+/**
+ * main - Prints all multiples of 3 or 5 up to 1024
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i, b = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			b += i;
+		}
+		i++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
