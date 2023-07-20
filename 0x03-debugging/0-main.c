fd818f40abd5f24990927d03241230efdb8d41f7
nyanchamaB <nyanchamabrendah3@gmail.com>
2023-07-20 13:31:37 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,6 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-*
 * Return: 0
 */
 
@@ -12,6 +11,5 @@ int main(void)
 
         i = 98;
         positive_or_negative(i);
-
         return (0);
 }
