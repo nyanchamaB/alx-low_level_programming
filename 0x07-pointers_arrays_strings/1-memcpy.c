ee7cb32bed493d085503073108964dce18e04140
nyanchamaB <nyanchamabrendah3@gmail.com>
2023-08-02 00:24:49 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memcpy - entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: 0 (success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
