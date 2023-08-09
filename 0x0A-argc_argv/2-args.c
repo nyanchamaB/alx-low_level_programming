e01843a3ac1bc9532355b096817d737da7129bfb
nyanchamaB <nyanchamabrendah3@gmail.com>
2023-08-09 09:55:30 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,12 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
-        int a = 0;
+	int a = 0;
 
-        while (a < argc)
-        {
-                printf("%s\n", argv[a]);
-                        a++;
-        }
-        return (0);
+	while (a < argc)
+	{
+		printf("%s\n", argv[a]);
+		a++;
+	}
+	return (0);
 }
